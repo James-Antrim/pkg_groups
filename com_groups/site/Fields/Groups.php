2acb1afed77eab2bf7c2d50280b423eac58b7c5e
James Antrim <james.antrim@nm.thm.de>
2022-10-26 16:38:54 +0200

    Replaced stumps.

--- com_groups/site/Fields/Groups.php
+++ com_groups/site/Fields/Groups.php
@@ -1,10 +1,11 @@
 <?php
 /**
- * @package     THM\Groups\Fields\Groups
- * @subpackage
- *
- * @copyright   A copyright
- * @license     A "Slug" license name e.g. GPL2
+ * @package     Groups
+ * @extension   com_groups
+ * @author      James Antrim, <james.antrim@nm.thm.de>
+ * @copyright   2022 TH Mittelhessen
+ * @license     GNU GPL v.3
+ * @link        www.thm.de
  */
 
 namespace THM\Groups\Fields;
