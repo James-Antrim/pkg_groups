2598b2e4dab322ee1739b776b4c2cd58f79dded9
James Antrim <james.antrim@nm.thm.de>
2022-10-31 11:47:10 +0100

    Updates reference install sql to match development stand.

--- z/com_thm_groups/admin/sql/install.sql
+++ z/com_thm_groups/admin/sql/install.sql
@@ -1,195 +1,143 @@
-#
-Restructure this so that structure, DATA AND REFERENCES ARE separated
+# Restructure this so that structure, data and references are separated
 SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
-SET
-time_zone = "+00:00";
+SET time_zone = "+00:00";
 
 CREATE TABLE IF NOT EXISTS `#__groups_attribute_types` (
-    `id` INT(
-    11
-) UNSIGNED NOT NULL AUTO_INCREMENT,
-    `fieldID` INT (11) UNSIGNED NOT NULL,
-    `type` VARCHAR (100) NOT NULL,
-    `options` TEXT,
-    `message` VARCHAR (100) DEFAULT '',
-    PRIMARY KEY (`id`),
-    UNIQUE (`type`)
-    )
-    ENGINE = InnoDB
-    DEFAULT CHARSET = utf8mb4
-    COLLATE = utf8mb4_unicode_ci;
+  `id`      INT(11)      UNSIGNED NOT NULL AUTO_INCREMENT,
+  `fieldID` INT(11)      UNSIGNED NOT NULL,
+  `type`    VARCHAR(100)          NOT NULL,
+  `options` TEXT,
+  `message` VARCHAR(100)                   DEFAULT '',
+  PRIMARY KEY (`id`),
+  UNIQUE (`type`)
+)
+  ENGINE = InnoDB
+  DEFAULT CHARSET = utf8mb4
+  COLLATE = utf8mb4_unicode_ci;
 
 CREATE TABLE IF NOT EXISTS `#__groups_attributes` (
-    `id` INT(
-    11
-) UNSIGNED NOT NULL AUTO_INCREMENT,
-    `typeID` INT (11) UNSIGNED NOT NULL,
-    `label` VARCHAR (100) NOT NULL,
-    `showLabel` TINYINT (1) UNSIGNED NOT NULL DEFAULT 1,
-    `icon` VARCHAR (255) NOT NULL DEFAULT '',
-    `showIcon` TINYINT (1) UNSIGNED NOT NULL DEFAULT 1,
-    `options` TEXT,
-    `ordering` INT (3) UNSIGNED NOT NULL DEFAULT 0,
-    `published` TINYINT (1) UNSIGNED NOT NULL DEFAULT 1,
-    `required` TINYINT (1) UNSIGNED NOT NULL DEFAULT 0,
-    `viewLevelID` INT (10) UNSIGNED DEFAULT 1,
-    PRIMARY KEY (`id`),
-    UNIQUE (`label`)
-    )
-    ENGINE = InnoDB
-    DEFAULT CHARSET = utf8mb4
-    COLLATE = utf8mb4_unicode_ci;
+  `id`          INT(11)      UNSIGNED NOT NULL AUTO_INCREMENT,
+  `typeID`      INT(11)      UNSIGNED NOT NULL,
+  `label`       VARCHAR(100)          NOT NULL,
+  `showLabel`   TINYINT(1)   UNSIGNED NOT NULL DEFAULT 1,
+  `icon`        VARCHAR(255)          NOT NULL DEFAULT '',
+  `showIcon`    TINYINT(1) UNSIGNED   NOT NULL DEFAULT 1,
+  `options`     TEXT,
+  `ordering`    INT(3) UNSIGNED       NOT NULL DEFAULT 0,
+  `published`   TINYINT(1) UNSIGNED   NOT NULL DEFAULT 1,
+  `required`    TINYINT(1) UNSIGNED   NOT NULL DEFAULT 0,
+  `viewLevelID` INT(10) UNSIGNED               DEFAULT 1,
+  PRIMARY KEY (`id`),
+  UNIQUE (`label`)
+)
+  ENGINE = InnoDB
+  DEFAULT CHARSET = utf8mb4
+  COLLATE = utf8mb4_unicode_ci;
 
-#
-profileID & categoryID remain signed because OF the users AND categories TABLE dependencies
+# profileID & categoryID remain signed because of the users and categories table dependencies
 CREATE TABLE IF NOT EXISTS `#__groups_categories` (
-    `id` INT(
-    11
-) NOT NULL,
-    `profileID` INT (11) NOT NULL,
-    PRIMARY KEY (`id`)
-    )
-    ENGINE = InnoDB
-    DEFAULT CHARSET = utf8mb4
-    COLLATE = utf8mb4_unicode_ci;
+  `id`        INT(11) NOT NULL,
+  `profileID` INT(11) NOT NULL,
+  PRIMARY KEY (`id`)
+)
+  ENGINE = InnoDB
+  DEFAULT CHARSET = utf8mb4
+  COLLATE = utf8mb4_unicode_ci;
 
-#
-profileID remains signed because OF the users TABLE dependency
+# profileID remains signed because of the users table dependency
 CREATE TABLE IF NOT EXISTS `#__groups_content` (
-    `id` INT(
-    11
-) UNSIGNED NOT NULL,
-    `profileID` INT (11) NOT NULL,
-    `featured` TINYINT (1) UNSIGNED NOT NULL DEFAULT 0,
-    PRIMARY KEY (`id`)
-    )
-    ENGINE = InnoDB
-    DEFAULT CHARSET = utf8mb4
-    COLLATE = utf8mb4_unicode_ci;
+  `id`        INT(11)    UNSIGNED NOT NULL,
+  `profileID` INT(11)             NOT NULL,
+  `featured`  TINYINT(1) UNSIGNED NOT NULL DEFAULT 0,
+  PRIMARY KEY (`id`)
+)
+  ENGINE = InnoDB
+  DEFAULT CHARSET = utf8mb4
+  COLLATE = utf8mb4_unicode_ci;
 
 CREATE TABLE IF NOT EXISTS `#__groups_fields` (
-    `id` INT(
-    11
-) UNSIGNED NOT NULL AUTO_INCREMENT,
-    `field` VARCHAR (20) NOT NULL,
-    `options` TEXT,
-    PRIMARY KEY (`id`),
-    UNIQUE (`field`)
-    )
-    ENGINE = INNODB
-    DEFAULT CHARSET = utf8mb4
-    COLLATE = utf8mb4_unicode_ci;
+  `id`      INT(11)      UNSIGNED NOT NULL AUTO_INCREMENT,
+  `field`   VARCHAR(20)           NOT NULL,
+  `options` TEXT,
+  PRIMARY KEY (`id`),
+  UNIQUE (`field`)
+)
+  ENGINE = INNODB
+  DEFAULT CHARSET = utf8mb4
+  COLLATE = utf8mb4_unicode_ci;
 
-#
-profileID remains signed because OF the users TABLE dependency
+# profileID remains signed because of the users table dependency
 CREATE TABLE IF NOT EXISTS `#__groups_profile_associations` (
-    `id` INT(
-    11
-) UNSIGNED NOT NULL AUTO_INCREMENT,
-    `profileID` INT (11) NOT NULL,
-    `role_associationID` INT (11) UNSIGNED NOT NULL,
-    PRIMARY KEY (`ID`)
-    )
-    ENGINE = InnoDB
-    DEFAULT CHARSET = utf8mb4
-    COLLATE = utf8mb4_unicode_ci;
+  `id`                 INT(11) UNSIGNED NOT NULL AUTO_INCREMENT,
+  `profileID`          INT(11)          NOT NULL,
+  `role_associationID` INT(11) UNSIGNED NOT NULL,
+  PRIMARY KEY (`ID`)
+)
+  ENGINE = InnoDB
+  DEFAULT CHARSET = utf8mb4
+  COLLATE = utf8mb4_unicode_ci;
 
-#
-profileID remains signed because OF the users TABLE dependency
+# profileID remains signed because of the users table dependency
 CREATE TABLE IF NOT EXISTS `#__groups_profile_attributes` (
-    `id` INT(
-    11
-) UNSIGNED NOT NULL AUTO_INCREMENT,
-    `profileID` INT (11) NOT NULL,
-    `attributeID` INT (11) UNSIGNED NOT NULL,
-    `value` TEXT,
-    `published` TINYINT (1) UNSIGNED NOT NULL DEFAULT 0,
-    PRIMARY KEY (`ID`)
-    )
-    ENGINE = InnoDB
-    DEFAULT CHARSET = utf8mb4
-    COLLATE = utf8mb4_unicode_ci;
+  `id`          INT(11)     UNSIGNED NOT NULL AUTO_INCREMENT,
+  `profileID`   INT(11)              NOT NULL,
+  `attributeID` INT(11)     UNSIGNED NOT NULL,
+  `value`       TEXT,
+  `published`   TINYINT(1)  UNSIGNED NOT NULL DEFAULT 0,
+  PRIMARY KEY (`ID`)
+)
+  ENGINE = InnoDB
+  DEFAULT CHARSET = utf8mb4
+  COLLATE = utf8mb4_unicode_ci;
 
-#
-id remains signed because OF the users TABLE dependency
+# id remains signed because of the users table dependency
 CREATE TABLE IF NOT EXISTS `#__groups_profiles` (
-    `id` INT(
-    11
-) NOT NULL,
-    `published` TINYINT (1) UNSIGNED NOT NULL DEFAULT 0,
-    `canEdit` TINYINT (1) UNSIGNED NOT NULL DEFAULT 0,
-    `contentEnabled` TINYINT (1) UNSIGNED NOT NULL DEFAULT 0,
-    `alias` VARCHAR (190) NOT NULL DEFAULT '',
-    PRIMARY KEY (`id`),
-    UNIQUE (`alias`)
-    )
-    ENGINE = InnoDB
-    DEFAULT CHARSET = utf8mb4
-    COLLATE = utf8mb4_unicode_ci;
-
-CREATE TABLE IF NOT EXISTS `#__groups_role_associations` (
-    `id` INT(
-    11
-) UNSIGNED NOT NULL AUTO_INCREMENT,
-    `groupID` INT (11) UNSIGNED NOT NULL,
-    `roleID` INT (11) UNSIGNED NOT NULL,
-    PRIMARY KEY (`id`)
-    )
-    ENGINE = InnoDB
-    DEFAULT CHARSET = utf8mb4
-    COLLATE = utf8mb4_unicode_ci;
-
-CREATE TABLE IF NOT EXISTS `#__groups_roles` (
-    `id` INT(
-    11
-) UNSIGNED NOT NULL AUTO_INCREMENT,
-    `name` VARCHAR (100) NOT NULL,
-    `ordering` INT (3) UNSIGNED NOT NULL DEFAULT 0,
-    PRIMARY KEY (`id`),
-    UNIQUE (`name`)
-    )
-    ENGINE = InnoDB
-    DEFAULT CHARSET = utf8mb4
-    COLLATE = utf8mb4_unicode_ci;
+  `id`             INT(11)             NOT NULL,
+  `published`      TINYINT(1) UNSIGNED NOT NULL DEFAULT 0,
+  `canEdit`        TINYINT(1) UNSIGNED NOT NULL DEFAULT 0,
+  `contentEnabled` TINYINT(1) UNSIGNED NOT NULL DEFAULT 0,
+  `alias`          VARCHAR(190)        NOT NULL DEFAULT '',
+  PRIMARY KEY (`id`),
+  UNIQUE (`alias`)
+)
+  ENGINE = InnoDB
+  DEFAULT CHARSET = utf8mb4
+  COLLATE = utf8mb4_unicode_ci;
 
 CREATE TABLE IF NOT EXISTS `#__groups_template_attributes` (
-    `id` INT(
-    11
-) UNSIGNED NOT NULL AUTO_INCREMENT,
-    `templateID` INT (11) UNSIGNED NOT NULL,
-    `attributeID` INT (11) UNSIGNED NOT NULL,
-    `published` TINYINT (1) UNSIGNED NOT NULL DEFAULT 1,
-    `ordering` INT (11) UNSIGNED NOT NULL DEFAULT 0,
-    `showLabel` TINYINT (1) UNSIGNED NOT NULL DEFAULT 1,
-    `showIcon` TINYINT (1) UNSIGNED NOT NULL DEFAULT 1,
-    PRIMARY KEY (`id`)
-    )
-    ENGINE = InnoDB
-    DEFAULT CHARSET = utf8mb4
-    COLLATE = utf8mb4_unicode_ci;
+  `id`          INT(11)      UNSIGNED NOT NULL AUTO_INCREMENT,
+  `templateID`  INT(11)      UNSIGNED NOT NULL,
+  `attributeID` INT(11)      UNSIGNED NOT NULL,
+  `published`   TINYINT(1)   UNSIGNED NOT NULL DEFAULT 1,
+  `ordering`    INT(11)      UNSIGNED NOT NULL DEFAULT 0,
+  `showLabel`   TINYINT(1)   UNSIGNED NOT NULL DEFAULT 1,
+  `showIcon`    TINYINT(1)   UNSIGNED NOT NULL DEFAULT 1,
+  PRIMARY KEY (`id`)
+)
+  ENGINE = InnoDB
+  DEFAULT CHARSET = utf8mb4
+  COLLATE = utf8mb4_unicode_ci;
 
 CREATE TABLE IF NOT EXISTS `#__groups_templates` (
-    `id` INT(
-    11
-) UNSIGNED NOT NULL AUTO_INCREMENT,
-    `templateName` VARCHAR (100) NOT NULL,
-    PRIMARY KEY (`id`),
-    UNIQUE (`templateName`)
-    )
-    ENGINE = InnoDB
-    DEFAULT CHARSET = utf8mb4
-    COLLATE = utf8mb4_unicode_ci;
+  `id`           INT(11)      UNSIGNED NOT NULL AUTO_INCREMENT,
+  `templateName` VARCHAR(100)          NOT NULL,
+  PRIMARY KEY (`id`),
+  UNIQUE (`templateName`)
+)
+  ENGINE = InnoDB
+  DEFAULT CHARSET = utf8mb4
+  COLLATE = utf8mb4_unicode_ci;
 
 INSERT INTO `#__groups_attribute_types` (`id`, `type`, `fieldID`, `message`, `options`)
-VALUES (1, 'Einfaches Text', 1, 'COM_THM_GROUPS_INVALID_TEXT', '{}'),
+VALUES (1, 'Einfaches Text', 1, 'COM_GROUPS_INVALID_TEXT', '{}'),
        (2, 'Ausführlicher Text / HTML', 2, '', '{}'),
-       (3, 'Link', 3, 'COM_THM_GROUPS_INVALID_URL', '{}'),
+       (3, 'Link', 3, 'COM_GROUPS_INVALID_URL', '{}'),
        (4, 'Bild', 4, '', '{"accept":".bmp,.BMP,.gif,.GIF,.jpg,.JPG,.jpeg,.JPEG,.png,.PNG"}'),
-       (5, 'Datum (EU)', 5, 'COM_THM_GROUPS_INVALID_DATE_EU', '{"calendarformat":"%d.%m.%Y","regex":"european_date"}'),
-       (6, 'E-Mail', 6, 'COM_THM_GROUPS_INVALID_EMAIL', '{}'),
-       (7, 'Telefon (EU)', 7, 'COM_THM_GROUPS_INVALID_TELEPHONE', '{"regex":"european_telephone"}'),
-       (8, 'Name', 1, 'COM_THM_GROUPS_INVALID_NAME', '{"regex":"name"}'),
-       (9, 'Namenszusatz', 1, 'COM_THM_GROUPS_INVALID_NAME_SUPPLEMENT', '{"regex":"name_supplement"}');
+       (5, 'Datum (EU)', 5, 'COM_GROUPS_INVALID_DATE_EU', '{"calendarformat":"%d.%m.%Y","regex":"european_date"}'),
+       (6, 'E-Mail', 6, 'COM_GROUPS_INVALID_EMAIL', '{}'),
+       (7, 'Telefon (EU)', 7, 'COM_GROUPS_INVALID_TELEPHONE', '{"regex":"european_telephone"}'),
+       (8, 'Name', 1, 'COM_GROUPS_INVALID_NAME', '{"regex":"name"}'),
+       (9, 'Namenszusatz', 1, 'COM_GROUPS_INVALID_NAME_SUPPLEMENT', '{"regex":"name_supplement"}');
 
 INSERT INTO `#__groups_attributes` (`id`, `typeID`, `label`, `showLabel`, `icon`, `showIcon`, `options`, `ordering`, `published`, `required`, `viewLevelID`)
 VALUES (3, 4, 'Bild', 0, '', 0, '{"mode":1}', 1, 1, 0, 1),
@@ -204,8 +152,7 @@ VALUES (3, 4, 'Bild', 0, '', 0, '{"mode":1}', 1, 1, 0, 1),
        (10, 1, 'Raum', 1, 'icon-home', 1, '{"hint":"A1.0.01"}', 10, 1, 0, 1);
 
 INSERT INTO `#__groups_fields` (`id`, `field`, `options`)
-VALUES (1, 'text', '{"maxlength":"255","hint":"","regex":"simple_text"}'),
-       (2, 'editor', '{"buttons":1,"hide":"ebevent,ebregister,groups_profiles,snippets,betterpreview,sliders,thmvcard,thmcontact,widgetkit,module,menu,contact,fields,jresearch_automatic_bibliography_generation,jresearch_automatic_citation,modals,pagebreak,readmore"}'),
+VALUES (2, 'editor', '{"buttons":1,"hide":"ebevent,ebregister,thm_groups_profiles,snippets,betterpreview,sliders,thmvcard,thmcontact,widgetkit,module,menu,contact,fields,jresearch_automatic_bibliography_generation,jresearch_automatic_citation,modals,pagebreak,readmore"}'),
        (3, 'URL', '{"maxlength":"255","hint":"","validate":1,"regex":"url"}'),
        (4, 'file', '{"accept":"","mode":1}'),
        (5, 'calendar', '{"calendarformat":"","showtime":"0","timeformat":"24","regex":""}'),
@@ -228,90 +175,61 @@ INSERT INTO `#__groups_templates` (`id`, `templateName`)
 VALUES (1, 'Default');
 
 ALTER TABLE `#__groups_attribute_types`
-    ADD CONSTRAINT `attributetypes_fieldid` FOREIGN KEY (`fieldID`) REFERENCES `#__groups_fields` (`id`)
-        ON UPDATE CASCADE
-        ON DELETE CASCADE;
+  ADD CONSTRAINT `attributetypes_fieldid` FOREIGN KEY (`fieldID`) REFERENCES `#__groups_fields` (`id`)
+  ON UPDATE CASCADE
+  ON DELETE CASCADE;
 
 ALTER TABLE `#__groups_attributes`
-    ADD CONSTRAINT `attributes_typeid` FOREIGN KEY (`typeID`)
-        REFERENCES `#__groups_attribute_types` (`id`)
-        ON UPDATE CASCADE
-        ON DELETE CASCADE,
+  ADD CONSTRAINT `attributes_typeid` FOREIGN KEY (`typeID`)
+REFERENCES `#__groups_attribute_types` (`id`)
+  ON UPDATE CASCADE
+  ON DELETE CASCADE,
   ADD CONSTRAINT `attributes_viewlevelid` FOREIGN KEY (`viewLevelID`) REFERENCES `#__viewlevels` (`id`)
-  ON
-DELETE
-CASCADE
-  ON
-UPDATE CASCADE;
+  ON DELETE CASCADE
+  ON UPDATE CASCADE;
 
 ALTER TABLE `#__groups_categories`
-    ADD CONSTRAINT `categories_categoryid` FOREIGN KEY (`id`) REFERENCES `#__categories` (`id`)
-        ON UPDATE CASCADE
-        ON DELETE CASCADE,
+  ADD CONSTRAINT `categories_categoryid` FOREIGN KEY (`id`) REFERENCES `#__categories` (`id`)
+  ON UPDATE CASCADE
+  ON DELETE CASCADE,
   ADD CONSTRAINT `categories_profileid` FOREIGN KEY (`profileID`) REFERENCES `#__groups_profiles` (`id`)
-  ON
-UPDATE CASCADE
-ON
-DELETE
-CASCADE;
+  ON UPDATE CASCADE
+  ON DELETE CASCADE;
 
 ALTER TABLE `#__groups_content`
-    ADD CONSTRAINT `content_contentid` FOREIGN KEY (`id`) REFERENCES `#__content` (`id`)
-        ON UPDATE CASCADE
-        ON DELETE CASCADE,
+  ADD CONSTRAINT `content_contentid` FOREIGN KEY (`id`) REFERENCES `#__content` (`id`)
+  ON UPDATE CASCADE
+  ON DELETE CASCADE,
   ADD CONSTRAINT `content_profileid` FOREIGN KEY (`profileID`) REFERENCES `#__groups_profiles` (`id`)
-  ON
-UPDATE CASCADE
-ON
-DELETE
-CASCADE;
+  ON UPDATE CASCADE
+  ON DELETE CASCADE;
 
 ALTER TABLE `#__groups_profile_associations`
-    ADD CONSTRAINT `profileassociations_profileid` FOREIGN KEY (`profileID`) REFERENCES `#__groups_profiles` (`id`)
-        ON UPDATE CASCADE
-        ON DELETE CASCADE,
+  ADD CONSTRAINT `profileassociations_profileid` FOREIGN KEY (`profileID`) REFERENCES `#__groups_profiles` (`id`)
+  ON UPDATE CASCADE
+  ON DELETE CASCADE,
   ADD CONSTRAINT `profileassociations_roleassociationid` FOREIGN KEY (`role_associationID`)
 REFERENCES `#__groups_role_associations` (`id`)
-  ON
-UPDATE CASCADE
-ON
-DELETE
-CASCADE;
+  ON UPDATE CASCADE
+  ON DELETE CASCADE;
 
 ALTER TABLE `#__groups_profile_attributes`
-    ADD CONSTRAINT `profileattributes_attributeid` FOREIGN KEY (`attributeID`) REFERENCES `#__groups_attributes` (`id`)
-        ON UPDATE CASCADE
-        ON DELETE CASCADE,
+  ADD CONSTRAINT `profileattributes_attributeid` FOREIGN KEY (`attributeID`) REFERENCES `#__groups_attributes` (`id`)
+  ON UPDATE CASCADE
+  ON DELETE CASCADE,
   ADD CONSTRAINT `profileattributes_profileid` FOREIGN KEY (`profileID`) REFERENCES `#__groups_profiles` (`id`)
-  ON
-UPDATE CASCADE
-ON
-DELETE
-CASCADE;
+  ON UPDATE CASCADE
+  ON DELETE CASCADE;
 
 ALTER TABLE `#__groups_profiles`
-    ADD CONSTRAINT `profiles_userid` FOREIGN KEY (`id`) REFERENCES `#__users` (`id`)
-        ON UPDATE CASCADE
-        ON DELETE CASCADE;
-
-ALTER TABLE `#__groups_role_associations`
-    ADD CONSTRAINT `roleassociations_roleid` FOREIGN KEY (`roleID`) REFERENCES `#__groups_roles` (`id`)
-        ON DELETE CASCADE
-        ON UPDATE CASCADE,
-  ADD CONSTRAINT `roleassociations_groupid` FOREIGN KEY (`groupID`) REFERENCES `#__usergroups` (`id`)
-  ON
-DELETE
-CASCADE
-  ON
-UPDATE CASCADE;
+  ADD CONSTRAINT `profiles_userid` FOREIGN KEY (`id`) REFERENCES `#__users` (`id`)
+  ON UPDATE CASCADE
+  ON DELETE CASCADE;
 
 ALTER TABLE `#__groups_template_attributes`
-    ADD CONSTRAINT `templateattributes_templateid` FOREIGN KEY (`templateID`) REFERENCES `#__groups_templates` (`id`)
-        ON UPDATE CASCADE
-        ON DELETE CASCADE,
+  ADD CONSTRAINT `templateattributes_templateid` FOREIGN KEY (`templateID`) REFERENCES `#__groups_templates` (`id`)
+  ON UPDATE CASCADE
+  ON DELETE CASCADE,
   ADD CONSTRAINT `templateattributes_attributeid` FOREIGN KEY (`attributeID`) REFERENCES `#__groups_attributes` (`id`)
-  ON
-UPDATE CASCADE
-ON
-DELETE
-CASCADE;
+  ON UPDATE CASCADE
+  ON DELETE CASCADE;
