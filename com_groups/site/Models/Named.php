4de165ce5d7007660412c18685304556ed46b217
James Antrim <james.antrim@nm.thm.de>
2023-10-17 12:07:28 +0200

    Parity organizer.

--- com_groups/site/Models/Named.php
+++ com_groups/site/Models/Named.php
@@ -12,39 +12,26 @@ namespace THM\Groups\Models;
 
 use THM\Groups\Adapters\Application;
 
+/**
+ * Class standardizes the getName function across classes.
+ */
 trait Named
 {
-	/**
-	 * Context string for the model type.  This is used to handle uniqueness
-	 * when dealing with the getStoreId() method and caching data structures.
-	 *
-	 * @var    string
-	 */
-	protected $context;
+    /**
+     * The name of the called class
+     */
+    protected $name;
 
-	/**
-	 * The model (base) name
-	 *
-	 * @var    string
-	 */
-	protected $name;
+    /**
+     * Method to get the model name.
+     * @return  string  the name of the model
+     */
+    public function getName(): string
+    {
+        if (empty($this->name)) {
+            $this->name = Application::getClass($this);
+        }
 
-	/**
-	 * Method to get the model name
-	 *
-	 * The model name. By default parsed using the classname or it can be set
-	 * by passing a $config['name'] in the class constructor
-	 *
-	 * @return  string  The name of the model
-	 */
-	public function getName(): string
-	{
-		if (empty($this->name))
-		{
-			$this->name    = Application::getClass($this);
-			$this->context = strtolower('com_groups.' . $this->getName());
-		}
-
-		return $this->name;
-	}
+        return $this->name;
+    }
 }
