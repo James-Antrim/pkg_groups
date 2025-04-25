76893d39686fda6a3bbbbb5adc4cdf26cc8b7c26
James Antrim <james.antrim@nm.thm.de>
2025-04-25 16:42:40 +0200

    Updates for form model from organizer.

--- com_groups/site/Models/FormModel.php
+++ com_groups/site/Models/FormModel.php
@@ -12,26 +12,15 @@ namespace THM\Groups\Models;
 
 use Exception;
 use Joomla\CMS\Form\Form as FormAlias;
-use Joomla\CMS\MVC\Model\FormModel as Base;
-use THM\Groups\Adapters\{Application, Form, FormFactory, MVCFactory};
+use Joomla\CMS\MVC\Model\FormModel as Core;
+use THM\Groups\Adapters\{Application, FormFactory, MVCFactory};
 
-/**
- * Model for data to be used with a form.
- */
-abstract class FormModel extends Base
+/** @inheritDoc */
+abstract class FormModel extends Core
 {
     use Named;
 
-    /**
-     * Constructor
-     *
-     * @param array       $config               An array of configuration options (name, state, dbo, table_path,
-     *                                          ignore_request).
-     * @param MVCFactory  $factory              The factory.
-     * @param FormFactory $formFactory          The form factory.
-     *
-     * @throws Exception
-     */
+    /** @inheritDoc */
     public function __construct($config, MVCFactory $factory, FormFactory $formFactory)
     {
         parent::__construct($config, $factory, $formFactory);
@@ -39,26 +28,15 @@ abstract class FormModel extends Base
         $this->setContext();
     }
 
-    /**
-     * Filters out form inputs which should not be displayed due to previous selections.
-     *
-     * @param Form $form the form to be filtered
-     *
-     * @return void modifies $form
-     */
-    protected function filterForm(Form $form)
-    {
-        // Per default no fields are altered
-    }
-
     /** @inheritDoc */
-    public function getForm($data = array(), $loadData = true): ?FormAlias
+    public function getForm($data = [], $loadData = true): ?FormAlias
     {
         $options = ['control' => '', 'load_data' => $loadData];
 
         try {
             return $this->loadForm($this->context, strtolower($this->name), $options);
-        } catch (Exception $exception) {
+        }
+        catch (Exception $exception) {
             Application::handleException($exception);
         }
 
