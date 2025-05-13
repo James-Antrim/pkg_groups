55551a92f5f8c3cdf073dc9478566aefda659c30
James Antrim <james.antrim@nm.thm.de>
2025-05-13 10:43:59 +0200

    Added provider files.

---
+++ mod_groups_profiles/services/provider.php
@@ -0,0 +1,24 @@
+<?php
+/**
+ * @package     Groups
+ * @extension   mod_groups_profiles
+ * @author      James Antrim, <james.antrim@nm.thm.de>
+ * @copyright   2025 TH Mittelhessen
+ * @license     GNU GPL v.3
+ * @link        www.thm.de
+ */
+
+defined('_JEXEC') or die;
+
+use Joomla\CMS\Extension\Service\Provider\{HelperFactory, Module, ModuleDispatcherFactory};
+use Joomla\DI\{Container, ServiceProviderInterface};
+
+return new class () implements ServiceProviderInterface {
+
+    public function register(Container $container): void
+    {
+        $container->registerServiceProvider(new ModuleDispatcherFactory('\\THM\\Module\\GroupsProfiles'));
+        $container->registerServiceProvider(new HelperFactory('\\THM\\Module\\GroupsProfiles\\Site\\Helper'));
+        $container->registerServiceProvider(new Module());
+    }
+};
